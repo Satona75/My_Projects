e66badd178ab36ccef4640fb3fe78ab3cab20832
Nick Sackey <nicksackey@hotmail.com>
2025-12-05 22:53:59 +0000

    Addition of Python

---
+++ Python/hello_world.py
@@ -0,0 +1,10 @@
+# This program says Hello and asks for my name
+
+print('Hello World!')
+print('What is your name?')
+myName = input()
+print('It is nice to meet you ' + myName)
+print('The length of your name is ' + str(len(myName)))
+print('How old are you?')
+myAge = input()
+print('You will be ' + str(int(myAge) + 1) + ' in a year')
