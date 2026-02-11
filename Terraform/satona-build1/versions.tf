a0b4499f61f918d8e674cb17697303e99a1b159f
Nick Sackey <nicksackey@hotmail.com>
2026-02-11 15:29:48 +0000

    Set up remote state for Terraform

--- Terraform/satona-build1/versions.tf
+++ Terraform/satona-build1/versions.tf
@@ -5,6 +5,12 @@ terraform {
       version = " ~> 4.35.0"
     }
   }
+  backend "azurerm" {
+      resource_group_name  = "rg-tfstate"
+      storage_account_name = "tfstate23864"
+      container_name       = "tfstate"
+      key                  = "satona.tfstate"
+  }
 }
 
 provider "azurerm" {
